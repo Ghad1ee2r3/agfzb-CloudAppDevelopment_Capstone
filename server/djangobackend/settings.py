7097ae56c4043c2ed1e5e0c3d74e6af766effa86
Ghadeer <apps02977@gmail.com>
2022-08-29 16:15:48 +0300

    add Login , Register and Logout

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["GH_best_car.au-syd.mybluemix.net"]
+ALLOWED_HOSTS = ["GH_best_car.au-syd.mybluemix.net" , '127.0.0.1']
 
 
 # Application definition
