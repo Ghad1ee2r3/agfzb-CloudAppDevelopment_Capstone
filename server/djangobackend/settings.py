66dd422b5ae5641d8e8b02df79a61dd344acaa82
Ghadeer <apps02977@gmail.com>
2022-08-29 16:53:47 +0300

    update manifest file

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["GH_best_car.au-syd.mybluemix.net" , '127.0.0.1']
+ALLOWED_HOSTS = [" GH_best_cars.au-syd.cf.appdomain.cloud"]
 
 
 # Application definition
